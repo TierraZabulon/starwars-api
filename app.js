801fabb5c51e23fd96931facc66f233797611aa8
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-08-27 15:45:18 -0400

    updated style and index

--- app.js
+++ app.js
@@ -14,6 +14,24 @@ const starWars = () => {
     .then((data) => {
       console.log(data);
       document.getElementById("display-el").innerText = data.results[0].name;
+      document.getElementById(
+        "height"
+      ).innerText = `Height: ${data.results[0].height}`;
+      document.getElementById(
+        "mass"
+      ).innerText = `Mass: ${data.results[0].mass}`;
+      document.getElementById(
+        "haircolor"
+      ).innerText = `Hair Color: ${data.results[0].hair_color}`;
+      document.getElementById(
+        "eyecolor"
+      ).innerText = `Eye Color: ${data.results[0].eye_color}`;
+      document.getElementById(
+        "birthyear"
+      ).innerText = `Birth Year: ${data.results[0].birth_year}`;
+      document.getElementById(
+        "gender"
+      ).innerText = `Gender: ${data.results[0].gender}`;
     })
     .catch((err) => {
       console.log(`error ${err}`);
